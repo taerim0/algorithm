d4bf97ff18bf3fab9f3c0000cffbe2113cc6e0da
rim <127942413+taerim0@users.noreply.github.com>
2023-10-29 01:11:19 +0900

    [Bronze V] Title: Cupcake Party, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int R, S;
+
+    cin >> R >> S;
+
+    R * 8 + S * 3 - 28 ? cout << R * 8 + S * 3 - 28 : cout << 0;
+}
