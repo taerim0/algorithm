add9950cfdf958466402b695943840c7c3cdb127
rim <127942413+taerim0@users.noreply.github.com>
2023-11-01 02:46:42 +0900

    [Gold IV] Title: Bad Cowtractors, Time: 12 ms, Memory: 3492 KB -BaekjoonHub

---
+++ 백준/Gold/7044. Bad Cowtractors/Bad Cowtractors.cc
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <queue>
+#define pll pair<int, int>
+
+using namespace std;
+
+int V, E;
+int ans;
+int sum;
+
+vector<pll> edge[1001];
+int dist[1001];
+bool check[1001];
+
+void find_mst()
+{
+	priority_queue<pll> pq;
+
+	check[1] = true;
+
+	for (int i = 0; i < edge[1].size(); i++)
+	{
+		auto [to, cost] = edge[1][i];
+
+		pq.push({ cost, to });
+	}
+
+	while (pq.size())
+	{
+		auto [dis, next] = pq.top();
+		pq.pop();
+
+		if (check[next])
+			continue;
+
+		check[next] = true;
+		dist[next] = dis;
+
+		for (int i = 0; i < edge[next].size(); i++)
+		{
+			auto [to, cost] = edge[next][i];
+
+			pq.push({ cost, to });
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> V >> E;
+
+	for (int i = 0, a, b, c; i < E; i++)
+	{
+		cin >> a >> b >> c;
+
+		sum += c;
+
+		edge[a].push_back({ b, c });
+		edge[b].push_back({ a, c });
+	}
+
+	find_mst();
+
+	for (int i = 2; i <= V; i++)
+	{
+		ans += dist[i];
+
+		if (dist[i] == 0)
+		{
+			cout << -1;
+			return 0;
+		}
+	}
+
+	cout << ans;
+}
