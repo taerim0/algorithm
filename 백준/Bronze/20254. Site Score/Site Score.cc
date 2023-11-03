cfb8d22167ab8e38598bc52c25596f323078e49b
rim <127942413+taerim0@users.noreply.github.com>
2023-11-03 09:03:20 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int a, b, c, d;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> a >> b >> c >> d;
+
+	cout << 56 * a + 24 * b + 14 * c + 6 * d;
+}
