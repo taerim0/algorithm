2412b632fab07e55a2f063b484b56491784f7db1
rim <127942413+taerim0@users.noreply.github.com>
2023-11-04 06:21:13 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+int x, y, z;
+
+int main()
+{
+	cin >> a >> b >> c;
+	cin >> x >> y >> z;
+
+	cout << x - c << " " << y / b << " " << z - a;
+}
