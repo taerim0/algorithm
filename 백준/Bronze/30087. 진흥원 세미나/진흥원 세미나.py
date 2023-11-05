90db5c197d991acf6db252e8d17ba9bfd533c30f
rim <127942413+taerim0@users.noreply.github.com>
2023-11-06 05:56:15 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,8 @@
+N = int(input())
+
+classroom = { "Algorithm" : 204, "DataAnalysis" : 207, "ArtificialIntelligence" : 302, "CyberSecurity" : "B101", "Network" : 303, "Startup" : 501, "TestStrategy" : 105}
+
+for i in range(N):
+    smn = input()
+    print(classroom[smn])
+
