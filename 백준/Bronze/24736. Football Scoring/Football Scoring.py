538d60be76318e8e8c4f8c32e99ece86e39a24d0
rim <127942413+taerim0@users.noreply.github.com>
2023-11-06 06:25:09 +0900

    [Bronze V] Title: Football Scoring, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.py
@@ -0,0 +1,5 @@
+a, b, c, d, e = map(int, input().split())
+ans1 = (a * 6 + b * 3 + c * 2 + d + e * 2)
+a, b, c, d, e = map(int, input().split())
+ans2 = (a * 6 + b * 3 + c * 2 + d + e * 2)
+print(str(ans1) + " " + str(ans2))
