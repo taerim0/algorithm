70dbaca0283c0afc0a506317bc6d9ea374cc0a34
rim <127942413+taerim0@users.noreply.github.com>
2023-11-17 14:58:58 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int dp[1001];
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+	}
+
+	cout << dp[n];
+}
