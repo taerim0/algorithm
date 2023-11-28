68a32af76ab79dcc4d3388c63dd85f8635d1bab5
rim <127942413+taerim0@users.noreply.github.com>
2023-11-28 10:00:27 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int arr[8], now[8];
+bool isused[8];
+
+void make(int size)
+{
+    if (size == M)
+    {
+        for (int i = 0; i < M; i++)
+            cout << now[i] << " ";
+        cout << "\n";
+
+        return;
+    }
+
+    bool isUsed[10001] = { 0 };
+
+    for (int i = 0; i < N; i++)
+    {
+        if (isUsed[arr[i]] || isused[i])
+            continue;
+
+        isUsed[arr[i]] = true;
+        isused[i] = true;
+        now[size] = arr[i];
+        make(size + 1);
+        isused[i] = false;
+    }
+
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + N);
+
+    make(0);
+}
