ed044f6cf997d6c8a80f64fa3f1e33621beb0cd9
rim <127942413+taerim0@users.noreply.github.com>
2023-11-28 09:50:47 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int arr[8], now[8];
+
+void make(int size, int nowmax)
+{
+    if (size == M)
+    {
+        for (int i = 0; i < M; i++)
+            cout << now[i] << " ";
+        cout << "\n";
+
+        return;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        if (arr[i] <= nowmax)
+            continue;
+
+        now[size] = arr[i];
+        make(size + 1, arr[i]);
+    }
+
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + N);
+
+    make(0, 0);
+}
