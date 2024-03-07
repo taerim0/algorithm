4f7fce8658830c0f31de55b3bb0b1993dbbfc716
rim <127942413+taerim0@users.noreply.github.com>
2024-03-07 17:33:40 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    char a;
+
+    cin >> a;
+
+    if (a == 'N' || a == 'n')
+        cout << "Naver D2";
+    else
+        cout << "Naver Whale";
+        
+}
